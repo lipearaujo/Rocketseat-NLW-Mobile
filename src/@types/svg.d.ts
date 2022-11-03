1fa4655dd07e2ee13f7509cfebf08bd45b657d4a
lipearaujo <filipecensi@hotmail.com>
2022-11-03 01:52:22 -0300

    Versao Mobile NLW Rocketseat

---
+++ src/@types/svg.d.ts
@@ -0,0 +1,8 @@
+// This file was created so that we could create our own types of files
+// This code snippet was taken from https://github.com/kristerkari/react-native-svg-transformer
+declare module "*.svg" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
